f3a1c3f9ff4d87515a30f4ec495b85d226e77052
MichaelNInfotrack <69827766+MichaelNInfotrack@users.noreply.github.com>
2021-11-10 15:09:00 +1100

    creating a scenario for the login

---
+++ features/Dashboard.feature
@@ -0,0 +1,30 @@
+
+Feature: PlanIT Search Fearture 
+
+Scenario 1: Ensure that the user is able to search for an exisiting products
+
+  Given the dashboard page is displayed 
+  When I enter and Existing Project into the search bar 
+  Then the Project should get displayed 
+
+Scenario 2: Ensure that the user is getting displayed an error message when then user inputs a project that doesnt exisit
+
+
+  Given the dashboard page is displayed 
+  When I enters a Project that does not exisit into the search bar 
+  Then user should get displayed an error message 
+
+
+Scenario 3: Ensure that when I type via lot number that only that number lot is getting displayed
+
+  Given the dashboard page is displayed 
+  When I enter a lot number in the search bar 
+  And click search via lot number 
+  Then the lots that comply with the search should get displayed
+
+
+Scenario 4: Ensure that when I type via lot number that only that number lot is getting displayed
+
+  Given the dashboard page is displayed 
+  When I enter and Existing Project into the search bar 
+  Then the Project should get displayed 
--- step-definitions/simpleSite.js
+++ step-definitions/simpleSite.js
@@ -1,33 +1,43 @@
 const {client} = require('nightwatch-api');
 const {Given, When, Then} = require('cucumber');
-var LoginPage = client.page.demoObjects();
+var LoginPage = client.page.LoginPage();
+var DashboardPage = client.page.DashboardPage();
+
 
 Given('I open Open the PlanIT Login Screen', () =>{
     console.log("Foundation Step")
     return LoginPage
     .navigate()
+    .maximizeWindow()
 
 });
 
 When("I enter my Username", ()=>{
-    console.log("Action Step")
+    console.log("Action step")
+
     return LoginPage
-    .setValue('@Username',"")
+    .setValue('@locusernameInput',"PlanIT-Tester-1")
+
 });
 
 When("I enter my Password", ()=>{
-    console.log("Action Step")
-    // page.waitForElementPresent('@startHere').click('@startHere');
-    // return page.waitForElementPresent('@headingText').assert.elementPresent('@headingText');
+    console.log("Action step")
+
+    return LoginPage
+    .setValue('@locPasswordInput',"PlanIT-Tester-1")
 });
 
 When("Click Subimit", ()=>{
-    console.log("Action Step")
-    // page.waitForElementPresent('@startHere').click('@startHere');
-    // return page.waitForElementPresent('@headingText').assert.elementPresent('@headingText');
+    console.log("Action step")
+
+    return LoginPage
+    .click("@locSubmitBtn")
+  
 });
 
 Then("the user is should be displayed the PlanIT Dashboard Screen", ()=>{
     console.log("Validation step")
+    return DashboardPage
+    .waitForElementPresent("@locPlanITSearch", 1000, false, "Dashboard Search bar is visible")
 
 })
