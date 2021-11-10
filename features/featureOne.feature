485db8ee382d4177511fde43987dd9adb367fc2b
MichaelNInfotrack <69827766+MichaelNInfotrack@users.noreply.github.com>
2021-11-11 09:11:23 +1100

    Revert "commit change"
    This reverts commit 4dc137cc46646f84cf11ff18ff8c8c6dfade751b.

--- features/featureOne.feature
+++ features/featureOne.feature
@@ -1,7 +1,7 @@
 
 Feature: PlanIT Login Feature
 
-Scenario 1: Ensure that the user is able to login with  valid credentials
+Scenario1: Ensure that the user is able to login with  valid credentials
 
   Given I open Open the PlanIT Login Screen
   When I enter my Username
@@ -10,3 +10,15 @@ Scenario 1: Ensure that the user is able to login with  valid credentials
   Then the user is should be displayed the PlanIT Dashboard Screen
 
 
+
+
+
+Scenario2: Ensure that the user is able to login with  valid credentials
+
+
+
+  Given I open Open the PlanIT Login Screen
+  When I enter an Incorrect Username
+  When I enter my Incorrec Password
+  When Click Subimit
+  Then the user is should be displayed an error message 
--- step-definitions/dashboardStepDefinitions.js
+++ step-definitions/dashboardStepDefinitions.js
@@ -1,122 +1,122 @@
-// const {client} = require('nightwatch-api');
-// const {Given, When, Then,And} = require('cucumber');
-// var LoginPage = client.page.LoginPage();
-// var DashboardPage = client.page.DashboardPage();
+const {client} = require('nightwatch-api');
+const {Given, When, Then,And} = require('cucumber');
+var LoginPage = client.page.LoginPage();
+var DashboardPage = client.page.DashboardPage();
 
 
 
-// // Scenario 1 
-// // Scenario 1: Ensure that the user is able to search for an exisiting products
+// Scenario 1 
+// Scenario 1: Ensure that the user is able to search for an exisiting products
 
-// Given('that the user is logged in and the dashboard page is displayed', () =>{
-//     console.log("Foundation Step")
-//     return DashboardPage
-//     .waitForElementPresent("@locPlanITSearch", 1000, false, "Dashboard Search bar is visible")
+Given('that the user is logged in and the dashboard page is displayed', () =>{
+    console.log("Foundation Step")
+    return DashboardPage
+    .waitForElementPresent("@locPlanITSearch", 1000, false, "Dashboard Search bar is visible")
 
 
-// });
+});
 
-// When("I enter and Existing Project into the search bar", ()=>{
+When("I enter and Existing Project into the search bar", ()=>{
 
-//     return DashboardPage
-//         .setValue("@locPlanITSearch","Sanity")
+    return DashboardPage
+        .setValue("@locPlanITSearch","Sanity")
         
 
-// });
+});
 
-// Then("the Project should get displayed", ()=>{
-//     console.log("Validation step")
-//     return DashboardPage
-//     .waitForElementPresent("@locPlanITSearch", 1000, false, "Dashboard Search bar is visible")
+Then("the Project should get displayed", ()=>{
+    console.log("Validation step")
+    return DashboardPage
+    .waitForElementPresent("@locPlanITSearch", 1000, false, "Dashboard Search bar is visible")
 
-// })
-
-
-
-// // Scenario 2
-// // Scenario 2: Ensure that the user is getting displayed an error message when then user inputs a project that doesnt exisit
-
-// Given('the dashboard page is displayed', () =>{
-//     console.log("Foundation Step")
-//     return DashboardPage
-//     .navigate()
-//     .maximizeWindow()
-
-// });
-
-// When("I enter my Username", ()=>{
-
-//     return DashboardPage
-//     .setValue('@locusernameInput',"PlanIT-Tester-1")
-
-// });
-
-// Then("the user is should be displayed the PlanIT Dashboard Screen", ()=>{
-//     console.log("Validation step")
-//     return DashboardPage
-//     .waitForElementPresent("@locPlanITSearch", 1000, false, "Dashboard Search bar is visible")
-
-// })
-
-
-// // Scenario 3
-
-
-// Given('the dashboard page is displayed', () =>{
-//     console.log("Foundation Step")
-//     return LoginPage
-//     .navigate()
-//     .maximizeWindow()
-
-// });
-
-// When("I enter my Username", ()=>{
-
-//     return LoginPage
-//     .setValue('@locusernameInput',"PlanIT-Tester-1")
-
-// });
-
-
-// When("I enter my Username", ()=>{
-
-//     return LoginPage
-//     .setValue('@locusernameInput',"PlanIT-Tester-1")
-
-// });
-
-// Then("the user is should be displayed the PlanIT Dashboard Screen", ()=>{
-//     console.log("Validation step")
-//     return DashboardPage
-//     .waitForElementPresent("@locPlanITSearch", 1000, false, "Dashboard Search bar is visible")
-
-// })
+})
 
 
 
+// Scenario 2
+// Scenario 2: Ensure that the user is getting displayed an error message when then user inputs a project that doesnt exisit
+
+Given('the dashboard page is displayed', () =>{
+    console.log("Foundation Step")
+    return DashboardPage
+    .navigate()
+    .maximizeWindow()
+
+});
+
+When("I enter my Username", ()=>{
+
+    return DashboardPage
+    .setValue('@locusernameInput',"PlanIT-Tester-1")
+
+});
+
+Then("the user is should be displayed the PlanIT Dashboard Screen", ()=>{
+    console.log("Validation step")
+    return DashboardPage
+    .waitForElementPresent("@locPlanITSearch", 1000, false, "Dashboard Search bar is visible")
+
+})
 
 
-// // Scenario 4
+// Scenario 3
 
 
-// Given('the dashboard page is displayed', () =>{
-//     console.log("Foundation Step")
-//     return LoginPage
-//     .navigate()
-//     .maximizeWindow()
+Given('the dashboard page is displayed', () =>{
+    console.log("Foundation Step")
+    return LoginPage
+    .navigate()
+    .maximizeWindow()
 
-// });
+});
 
-// When("I enter my Username", ()=>{
+When("I enter my Username", ()=>{
 
-//     return LoginPage
-//     .setValue('@locusernameInput',"PlanIT-Tester-1")
+    return LoginPage
+    .setValue('@locusernameInput',"PlanIT-Tester-1")
 
-// });
+});
 
-// Then("the user is should be displayed the PlanIT Dashboard Screen", ()=>{
-//     console.log("Validation step")
-//     return DashboardPage
-//     .waitForElementPresent("@locPlanITSearch", 1000, false, "Dashboard Search bar is visible")
 
-// })
+When("I enter my Username", ()=>{
+
+    return LoginPage
+    .setValue('@locusernameInput',"PlanIT-Tester-1")
+
+});
+
+Then("the user is should be displayed the PlanIT Dashboard Screen", ()=>{
+    console.log("Validation step")
+    return DashboardPage
+    .waitForElementPresent("@locPlanITSearch", 1000, false, "Dashboard Search bar is visible")
+
+})
+
+
+
+
+
+// Scenario 4
+
+
+Given('the dashboard page is displayed', () =>{
+    console.log("Foundation Step")
+    return LoginPage
+    .navigate()
+    .maximizeWindow()
+
+});
+
+When("I enter my Username", ()=>{
+
+    return LoginPage
+    .setValue('@locusernameInput',"PlanIT-Tester-1")
+
+});
+
+Then("the user is should be displayed the PlanIT Dashboard Screen", ()=>{
+    console.log("Validation step")
+    return DashboardPage
+    .waitForElementPresent("@locPlanITSearch", 1000, false, "Dashboard Search bar is visible")
+
+})
