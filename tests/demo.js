dcb16c4c9910ffd2a22b8b1b545514b524b900e6
Satabdi Sikdar <satabdi.sikdar@velotio.com>
2023-08-17 18:37:28 +0530

    small change

--- tests/demo.js
+++ tests/demo.js
@@ -9,6 +9,6 @@ module.exports = {
     var page = browser.page.demoObjects();
     page.navigate().waitForElementVisible('body').assert.elementPresent('@header');
     page.click('@startHere').pause(10000).assert.elementPresent('@headingText');
-    page.end(); //to close the browser session after all the steps
+    page.end(); //to close the browser session at the end
   }
 }
